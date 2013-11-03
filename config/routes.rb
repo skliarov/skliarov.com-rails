414a7b31fd4440fc09001ccaa854c8f8a9c01fee
Maksym Skliarov <maksym.skliarov@gmail.com>
2013-11-03 14:58:41 +0200

    Edited default devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Consigliere::Application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   
-  devise_for :users
+  devise_for :users, :path_names => { :sign_up => "register", :sign_in => "signin", :sign_out => "signout" }
 
   root to: "articles#index"
   
