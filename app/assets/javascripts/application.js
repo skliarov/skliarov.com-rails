9e42b0fac8aebc274333e1754fea44139709ddef
Maksym Skliarov <maksym.skliarov@gmail.com>
2013-11-08 19:44:18 +0200

    Pretty articles#show, notices are in layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,13 +5,10 @@
 //= require bootstrap/bootstrap
 //= require syntaxhighliter/shCore.js
 //= require syntaxhighliter/shBrushBash.js
-//= require syntaxhighliter/shBrushAppleScript.js
 //= require syntaxhighliter/shBrushCpp.js
-//= require syntaxhighliter/shBrushCSharp.js
 //= require syntaxhighliter/shBrushCss.js
 //= require syntaxhighliter/shBrushJScript.js
 //= require syntaxhighliter/shBrushPlain.js
 //= require syntaxhighliter/shBrushRuby.js
-//= require syntaxhighliter/shBrushSass.js
 //= require syntaxhighliter/shBrushSql.js
 //= require syntaxhighliter/shBrushXml.js
