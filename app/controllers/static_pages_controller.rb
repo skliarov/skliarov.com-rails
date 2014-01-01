828389009b4711660af8b3eee5837470216aa259
Maksym Skliarov <maksym.skliarov@gmail.com>
2014-01-01 21:37:55 +0200

    Removed unnecessary views and methods from static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,40 +1,13 @@
 class StaticPagesController < ApplicationController
-  before_action :set_static_page, only: [:show, :edit, :update, :destroy]
-
-  # GET /static_pages
-  # GET /static_pages.json
-  def index
-    @static_pages = StaticPage.all
-  end
+  before_action :set_static_page, only: [:show, :edit]
 
   # GET /static_pages/1
   # GET /static_pages/1.json
   def show
   end
 
-  # GET /static_pages/new
-  def new
-    @static_page = StaticPage.new
-  end
-
   # GET /static_pages/1/edit
   def edit
-  end
-
-  # POST /static_pages
-  # POST /static_pages.json
-  def create
-    @static_page = StaticPage.new(static_page_params)
-
-    respond_to do |format|
-      if @static_page.save
-        format.html { redirect_to @static_page, notice: 'Static page was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @static_page }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @static_page.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /static_pages/1
@@ -48,16 +21,6 @@ class StaticPagesController < ApplicationController
         format.html { render action: 'edit' }
         format.json { render json: @static_page.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /static_pages/1
-  # DELETE /static_pages/1.json
-  def destroy
-    @static_page.destroy
-    respond_to do |format|
-      format.html { redirect_to static_pages_url }
-      format.json { head :no_content }
     end
   end
 
