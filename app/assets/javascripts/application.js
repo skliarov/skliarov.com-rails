174d405ca09152e8a0899eb8ec3d87a981c9a78e
Maksym Skliarov <maksym.skliarov@gmail.com>
2015-09-29 02:36:03 +0300

    Added missing portfolio items

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,37 @@
 //= require bootstrap-sprockets
 //= require owl.carousel
 //= require highlight/highlight.js
+
+$(document).ready(function() {
+  hljs.initHighlightingOnLoad();
+  
+  $(".owl-carousel-1").owlCarousel({
+    items: 1,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsTabletSmall: false,
+    itemsMobile: false,
+    itemsCustom: false
+  });
+  
+  $(".owl-carousel-2").owlCarousel({
+    items: 2,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsTabletSmall: false,
+    itemsMobile: false,
+    itemsCustom: [[400, 1], [640, 2]]
+  });
+  
+  $(".owl-carousel-3").owlCarousel({
+    items: 3,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsTabletSmall: false,
+    itemsMobile: false,
+    itemsCustom: [[400, 1], [640, 2], [800, 3]]
+  });
+});
