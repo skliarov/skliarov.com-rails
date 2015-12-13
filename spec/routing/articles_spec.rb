e2c959075703506f4063bfb13ecd2ef2e9161a01
Maksym Skliarov <maksym.skliarov@gmail.com>
2015-12-13 03:42:27 +0200

    Covered ArticlesController routes with tests

---
+++ spec/routing/articles_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, type: :routing do
+  it 'routes "/" to articles#index' do
+    expect(get('/')).to route_to('articles#index')
+  end
+  
+  it 'routes "/articles" to "articles#index"' do
+    expect(get('/articles')).to route_to('articles#index')
+  end
+  
+  it 'routes "/articles/:id" should route to "articles#show"' do
+    expect(get('/articles/1')).to route_to('articles#show', id: '1')
+  end
+end
