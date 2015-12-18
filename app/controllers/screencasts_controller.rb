169aaa68c603f8f21898f29f76f7053295e9f621
Maksym Skliarov <maksym.skliarov@gmail.com>
2015-12-18 02:23:41 +0200

    Implemented screncasts#indexw

--- app/controllers/screencasts_controller.rb
+++ app/controllers/screencasts_controller.rb
@@ -1,7 +1,9 @@
 class ScreencastsController < ApplicationController
   def index
+    @chapters = Chapter.all.order('position ASC')
   end
   
   def show
+    @screencast = Screencast.friendly.find(params[:id])
   end
 end
