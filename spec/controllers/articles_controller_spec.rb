286e344d116971b852be7d99725fc5b9eb56b24f
Maksym Skliarov <maksym.skliarov@gmail.com>
2015-12-23 04:18:41 +0200

    Tested articles controller

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, type: :controller do
+  before :all do
+    # Create User
+    @user = FactoryGirl.create(:user)
+    # Create test articles
+    article1 = FactoryGirl.create(:article, published: true, user: @user)
+    article2 = FactoryGirl.create(:article, published: false, user: @user)
+    article3 = FactoryGirl.create(:article, published: true, user: @user)
+    @articles = [article1, article2, article3]
+  end
+  
+  describe 'GET #index' do
+    before :each do
+      get :index
+    end
+    it 'should have successful response status' do
+      expect(response).to be_success
+    end
+    it 'should have valid content-type' do
+      expect(response.content_type).to eq('text/html')
+    end
+    it 'should render #index view' do
+      expect(response).to render_template(:index)
+    end
+  end
+  
+  describe 'GET #show' do
+    before :each do
+      article = @articles[0]
+      get :show, id: article.slug
+    end
+    it 'should have successful response status' do
+      expect(response).to be_success
+    end
+    it 'should have valid content-type' do
+      expect(response.content_type).to eq('text/html')
+    end
+    it 'should render #show view' do
+      expect(response).to render_template(:show)
+    end
+  end
+  
+  describe 'GET #feed' do
+    before :each do
+      get :feed, {format: 'rss'}
+    end
+    it 'should have successful response status' do
+      expect(response).to be_success
+    end
+    it 'should have valid content-type' do
+      expect(response.content_type).to eq('application/rss+xml')
+    end
+    it 'should render #feed view' do
+      expect(response).to render_template(:feed)
+    end
+  end
+end
