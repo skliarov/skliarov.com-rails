43dd6f5de07af4e0334485d7fe0e43b7e0ac4cce
Maksym Skliarov <maksym.skliarov@gmail.com>
2016-04-14 11:52:25 +0300

    Fixed spec/models

--- spec/factories/article.rb
+++ spec/factories/article.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :article do
     title { Faker::Lorem.sentence(5) }
+    slug { Faker::Internet.slug }
     preview { Faker::Lorem.paragraph(2) }
     body { Faker::Lorem.paragraph(50) }
     keywords { Faker::Lorem.words(5).join(',') }
