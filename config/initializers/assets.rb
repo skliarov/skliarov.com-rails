9d27bfa1ba5ae5e157aac3598e0ceee9e5ce67c1
Maksym Skliarov <maksym.skliarov@gmail.com>
2020-12-10 15:29:06 +0200

    Explicitly added images/* to Rails.application.config.assets.precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( application.js application.css admin.js admin.css Ckeditor.assets ckeditor/* )
+Rails.application.config.assets.precompile += %w( images/* application.js application.css admin.js admin.css Ckeditor.assets ckeditor/* )
